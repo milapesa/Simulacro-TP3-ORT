aafb9254ed506998fd24ae25df4a91571e8d5ec7
lucasmoix <129341492+lucasmoix@users.noreply.github.com>
2024-05-20 18:49:38 -0300

    esctructura inicial

--- app/src/main/java/com/example/simulacro_tp3_ort/ui/views/activities/ActivityHome.kt
+++ app/src/main/java/com/example/simulacro_tp3_ort/ui/views/activities/ActivityHome.kt
@@ -1,16 +1,17 @@
-package com.example.simulacro_tp3_ort
+package com.example.simulacro_tp3_ort.ui.views.activites
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.simulacro_tp3_ort.R
 
-class MainActivity : AppCompatActivity() {
+class ActivityHome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.lay_activity_home)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/example/simulacro_tp3_ort/ui/views/activities/ActivityWelcome.kt
@@ -0,0 +1,17 @@
+package com.example.simulacro_tp3_ort.ui.views.activites
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.simulacro_tp3_ort.R
+
+class ActivityWelcome : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.lay_activity_welcome)
+
+    }
+}
